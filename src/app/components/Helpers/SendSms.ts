ba8fae5c3e3187dc71d6c8db6d2f2ff87a5324b1
Aleksandr <sanek.miron2@gmail.com>
2025-12-16 22:10:32 +0300

    sms

--- src/app/components/Helpers/SendSms.ts
+++ src/app/components/Helpers/SendSms.ts
@@ -1,60 +1,67 @@
-import axios from 'axios'
-const BASE_URL = 'https://api.textbee.dev/api/v1'
-const API_KEY = process.env.TEXTBEE_API_KEY;
-const DEVICE_ID = process.env.TEXTBEE_DEVICE_ID;
-export async function sendSMS(to: string, text: string) {
-  const response = await axios.post(
-    `${BASE_URL}/gateway/devices/${DEVICE_ID}/send-sms`,
-    {
-      recipients: [to],
-      message: text
-    },
-    { headers: { 'x-api-key': API_KEY } }
-  )
+// import axios from 'axios'
+// const BASE_URL = 'https://api.textbee.dev/api/v1'
 
-  console.log(response.data)
-}
-
-// "use server";
-
-// const BASE_URL = "https://api.textbee.dev/api/v1";
-// const API_KEY = process.env.TEXTBEE_API_KEY; // ← правильное имя переменной
-// const DEVICE_ID = process.env.TEXTBEE_DEVICE_ID; // ← правильное имя
+// // EXTBEE_DEVICE_ID = 69415ab1fa641934d138904f
+// // TEXTBEE_API_KEY = 3646594d - 14db - 425f - 8361 - b6e560b0fc95
 
 // export async function sendSMS(to: string, text: string) {
-//   // Проверка env-переменных — обязательно!
-//   if (!API_KEY) {
-//     throw new Error("TEXTBEE_API_KEY не задан в .env");
-//   }
-//   if (!DEVICE_ID) {
-//     throw new Error("TEXTBEE_DEVICE_ID не задан в .env");
-//   }
-
-//   const response = await fetch(
-//     `${BASE_URL}/gateway/devices/${DEVICE_ID}/send-sms`,
+//   const response = await axios.post(
+//     `${BASE_URL}/gateway/devices/69415ab1fa641934d138904f/send-sms`,
 //     {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "x-api-key": API_KEY, // ← всё правильно!
-//       },
-//       body: JSON.stringify({
-//         recipients: [to],   // ← динамический номер
-//         message: text,      // ← динамический текст
-//       }),
-//     }
-//   );
+//       recipients: [to],
+//       message: text
+//     },
+//     { headers: { 'x-api-key': "3646594d-14db-425f-8361-b6e560b0fc95" } }
+//   )
 
-//   const data = await response.json();
-
-//   if (!response.ok) {
-//     console.error("Ошибка от TextBee:", data);
-//     throw new Error(`TextBee error ${response.status}: ${data.error || data.message || "Неизвестная ошибка"}`);
-//   }
-
-//   console.log("SMS успешно отправлено:", data);
-//   return data;
+//   console.log(response.data)
 // }
+
+"use server";
+
+// interface Data { message: string, recipientCount: 1, smsBatchId: string, success: boolean }
+// interface Response { data: Data }
+
+const BASE_URL = "https://api.textbee.dev/api/v1";
+
+export async function sendSMS(to: string, text: string) {
+
+  if (!process.env.TEXTBEE_API_KEY) {
+    throw new Error("TEXTBEE_API_KEY не задан в .env");
+  }
+  if (!process.env.TEXTBEE_DEVICE_ID) {
+    throw new Error("TEXTBEE_DEVICE_ID не задан в .env");
+  }
+
+  const response = await fetch(
+    `${BASE_URL}/gateway/devices/${process.env.TEXTBEE_DEVICE_ID}/send-sms`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.TEXTBEE_API_KEY,
+      },
+      body: JSON.stringify({
+        recipients: [to],
+        message: text,
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  if (data.data.success !== true) {
+    console.error("Ошибка от TextBee:", data);
+    throw new Error(`TextBee error ${response.status}: ${data.error || data.message || "Неизвестная ошибка"}`);
+  }
+
+  console.log("SMS успешно отправлено:", data);
+  return data;
+}
+
+
+
+
 // export async function sendSMS(to: string, text: string) {
 //   const res = await fetch("https://api.textbee.dev/v1/messages", {
 //     method: "POST",

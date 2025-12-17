9dd7dfc1ad092531a0936c3a0240acfe02de249f
Aleksandr <sanek.miron2@gmail.com>
2025-12-17 20:06:51 +0300

    fix

--- src/app/api/send-a-request-for-collaboration/route.ts
+++ src/app/api/send-a-request-for-collaboration/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { sendEmail } from "@/app/api/helpers/sendEmail"
 // import { readState, dataFile } from "../helpers/getState"; 
 // import { boolean } from "yup";
-import { sendSMS } from "@/app/api/helpers/sendSms";
+import { sendSMS } from "@/app/api/helpers/SendSms";
 
 export async function POST(req: Request) {
   const response = NextResponse.json({ success: true })

de04416f9156083f83fdf1acde940a211a3eb5aa
Aleksandr <sanek.miron2@gmail.com>
2025-12-19 02:15:00 +0300

    fix

--- src/app/components/OrderModal/OrderModal.tsx
+++ src/app/components/OrderModal/OrderModal.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import styles from "./OrderModal.module.scss"
 import * as yup from "yup";
 import { IMaskInput } from "react-imask";
-import { useForm, Controller, useWatch } from "react-hook-form";
+import { useForm, Controller, useWatch, useFormContext } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { OrderModalProps, FileObj, FormValues } from "../DTO/DTO"
 // import DownloadButton from "../DownloadButton/DownloadButton"
@@ -80,7 +80,7 @@ export default function OrderModal({ initialData, isOpen, onClose, alertNotifica
   const [showInvois, setShowInvois] = useState<boolean>(false) //открыты ли файлы флаг
   const [descriptionOfCargo, setDescriptionOfCargo] = useState<string>("")
 
-  const { register, handleSubmit, control, formState: { errors, isSubmitting,/*isValid,*/ }, setValue, getValues, trigger, watch, reset } = useForm<FormValues>({
+  const { register, handleSubmit, control, formState: { errors, /*isSubmitting,isValid,*/ }, setValue, getValues, trigger, watch, reset } = useForm<FormValues>({
     resolver: yupResolver(schema),
     mode: "onChange",
     reValidateMode: "onChange",//"onChange",
@@ -210,17 +210,21 @@ export default function OrderModal({ initialData, isOpen, onClose, alertNotifica
     ],
   });
 
-  const agree = useWatch({
-    control,
-    name: "agree",
-  });
 
-  const isFilled =
-    requiredFields.every(
-      (value) =>
-        (typeof value === "string" || typeof value === "number") &&
-        String(value).trim().length > 0
-    ) && agree === true;
+  const agree = useWatch({ control, name: "agree" });
+
+  // Проверяем, что все обязательные поля заполнены (не пустые) и нет ошибок по ним
+  const allFieldsFilled = requiredFields.every(v => (typeof v === "string" || typeof v === "number") && String(v).trim() !== "");
+
+  // Проверяем, что в errors нет ошибок для обязательных полей
+  const noErrorsInRequiredFields = requiredFields.every(field => !errors[field as keyof typeof errors]);
+
+  const isFilled = !!(allFieldsFilled && noErrorsInRequiredFields && Boolean(agree)
+    && descriptionOfCargo && from && where && indexFrom && indexWhere && showInvois)
+
+
+
+
   return (
     <>
       {isOpen &&

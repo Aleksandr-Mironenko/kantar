5d7b0666265eb03ebef37b22570e63eb30be8610
Aleksandr <sanek.miron2@gmail.com>
2026-03-10 16:29:47 +0300

    FULL WORK

--- src/app/api/client/orders/search-one-order/route.ts
+++ src/app/api/client/orders/search-one-order/route.ts
@@ -22,27 +22,31 @@ export async function POST(req: Request) {
 
     //ищу детали мест по номеру заказам
     const arrayPlacesInOrder = await findPlace(numberOrder)
-
+    console.log(arrayPlacesInOrder)
     //получение файлов из конкретного заказа
     const arrrfiles = await findFilesInOrder(numberOrder)
-
+    console.log(arrrfiles)
 
     //получние данных об отправителе
     const dataUserSendler = await findUser(dataOrder.sender_id)
+    console.log(dataUserSendler)
+
     //получение файлов отправителя
     const filesSendler = await findFilesInUser(dataOrder.sender_id)
+    console.log(filesSendler)
 
     //получние данных о получателе
     const dataUserRecipient = await findUser(dataOrder.recipient_id)
-
+    console.log(dataUserRecipient)
     const filesRecipient = await findFilesInUser(dataOrder.recipient_id)
+    console.log(filesRecipient)
 
     //получние данных адреса отправителя
     const dataAddressSendler = await findAddress(dataOrder.address_from_id)
-
+    console.log(dataAddressSendler)
     //получние данных адреса получателя
     const dataAddressRecipient = await findAddress(dataOrder.address_where_id)
-
+    console.log(dataAddressRecipient)
     // Возвращаем нужные поля
     return new Response(
       JSON.stringify({

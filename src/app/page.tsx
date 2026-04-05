7c047f893cd41f9bbdaa38a1554f510aeaeb7a76
Aleksandr-Mironenko <154741410+Aleksandr-Mironenko@users.noreply.github.com>
2026-04-05 20:10:14 +0300

    Refactor page.tsx to comment out unused components
    Comment out multiple component imports and usage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,30 @@
-// import Image from "next/image";
 
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import WhoWeAre from './components/WhoWeAre/WhoWeAre';
-// import Services from './components/Services/Services';
-import Advantages from './components/Advantages/Advantages';
-import Contacts from './components/Contacts/Contacts';
-import Footer from './components/Footer/Footer';
-import FormCalc from './components/FormCalc/FormCalc'
-import CalkSend from './components/CalkSend/CalkSend'
-import ThirdPartyFix from './components/ThirdPartyFix/ThirdPartyFix';
-import CooperationRequestForm from './components/CooperationRequestForm/CooperationRequestForm'
+// import Header from './components/Header/Header';
+// import Hero from './components/Hero/Hero';
+// import WhoWeAre from './components/WhoWeAre/WhoWeAre';
+// import Advantages from './components/Advantages/Advantages';
+// import Contacts from './components/Contacts/Contacts';
+// import Footer from './components/Footer/Footer';
+// import FormCalc from './components/FormCalc/FormCalc'
+// import CalkSend from './components/CalkSend/CalkSend'
+// import ThirdPartyFix from './components/ThirdPartyFix/ThirdPartyFix';
+// import CooperationRequestForm from './components/CooperationRequestForm/CooperationRequestForm'
 export default function Home() {
 
 
   return (
     <>
-      <ThirdPartyFix />
+    <p>Нет данных для отображения</p>
+      {/* <ThirdPartyFix />
       <Header />
       <Hero />
       <WhoWeAre />
-      {/* <Services /> */}
       <Advantages />
       <FormCalc />
       <Contacts />
       <CalkSend />
       <CooperationRequestForm />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
